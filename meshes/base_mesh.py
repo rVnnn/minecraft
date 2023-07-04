ef2d8bf83aba2b1488788045c9412e7df73796f1
Nils <nils-rausch@gmx.de>
2023-07-04 17:19:02 +0200

    Initial commit

---
+++ meshes/base_mesh.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class BaseMesh:
+    def __init__(self):
+        self.ctx = None
+        self.program = None
+        self.vbo_format = None
+        self.attrs: tuple[str, ...] = None
+        self.vao = None
+
+    def get_vertex_data(self) -> np.array:...
+
+    def get_vao(self):
+        vertex_data = self.get_vertex_data()
+        vbo = self.ctx.buffer(vertex_data)
+        vao = self.ctx.vertex_array(self.program, [(vbo, self.vbo_format, *self.attrs)], skip_errors = True)
+        return vao
+
+    def render(self):
+        self.vao.render()
